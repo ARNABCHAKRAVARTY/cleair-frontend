3d37f3b6b6cb33e87b18cfb73f063c119437f053
Arnab Majumdar <arnab.majumdar@arcvisions.com>
2020-04-12 14:07:52 +0530

    Added Location Dashboard to Home view tabs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,8 @@
 
     <v-tabs-items v-model="tab">
       <v-tab-item v-for="item in items" :key="item.key">
-        <dashboard v-if="item.key == 'locations'"></dashboard>        
+        <location-dashboard v-if="item.key == 'locations'"></location-dashboard>        
+        <device-dashboard v-if="item.key == 'devices'"></device-dashboard>        
       </v-tab-item>
     </v-tabs-items>
   </v-container>
@@ -18,7 +19,8 @@ export default {
     source: String
   },
   components: {
-    Dashboard: () => import("@/components/Dashboard")
+    LocationDashboard: () => import("@/components/Dashboard"),
+    DeviceDashboard: () => import("@/components/DeviceDashboard")
   },
 
   data: () => ({
